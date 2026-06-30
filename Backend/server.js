a69bdea124f8f19c1bbc6a4da3a86c77263b79e7
somya0827 <somyashukla2702@gmail.com>
2026-06-30 11:52:18 +0530

    Zod Schema and AI Response Structure

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const app = require('./src/app')
 const connectToDB = require("./src/config/db")
+// const {resume,selfDescription,jobDescription} = require('./src/services/temp')
+// const generateInterviewReport = require('./src/services/ai.service')
 
 connectToDB()
 
